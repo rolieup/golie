a6a36dfa666b9ef29fd9e336c822303d6a462b64
Šimon Lukašík <isimluk@fedoraproject.org>
2020-05-12 20:07:43 +0200

    Improve test to compare results of json and xml parsers

--- pkg/models/feed_test.go
+++ pkg/models/feed_test.go
@@ -1,29 +1,47 @@
 package models_test
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 
 	"github.com/rolieup/golie/pkg/models"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestParser_Parse(t *testing.T) {
+func TestCompareXMLandJSON(t *testing.T) {
 	xmlFile := "../../examples/rolie/feed.xml"
 	base := filepath.Base(xmlFile)
 	name := strings.TrimSuffix(base, filepath.Ext(base))
 
 	fmt.Printf("Testing parsing of %s... ", name)
-	bytes, err := ioutil.ReadFile(xmlFile)
+	xmlReader, err := os.Open(xmlFile)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(bytes)
-	err = xml.Unmarshal(bytes, &(models.Feed{}))
+	xmlParsed := &models.Feed{}
+	decoder := xml.NewDecoder(xmlReader)
+	err = decoder.Decode(xmlParsed)
 	if err != nil {
 		panic(err)
 	}
+
+	jsonFile := "../../examples/rolie/feed.json"
+	jsonParsedRoot := &models.JSONFeedRoot{}
+	jsonBytes, err := ioutil.ReadFile(jsonFile)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(jsonBytes, jsonParsedRoot)
+	if err != nil {
+		panic(err)
+	}
+	jsonParsed := &jsonParsedRoot.Feed
+	jsonParsed.XMLName = xmlParsed.XMLName // Disregard that json parser did not populate XMLName
+	assert.Equal(t, xmlParsed, jsonParsed, "XML parser returns different data than json equivalent")
 }
