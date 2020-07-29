25047a781ec25fa643111a6a00b5289985c00089
Šimon Lukašík <isimluk@fedoraproject.org>
2020-07-29 14:54:39 +0200

    Fix various exporting issues that lead to invalid Rolie documents

--- pkg/models/marshal_xml.go
+++ pkg/models/marshal_xml.go
@@ -9,6 +9,7 @@ const (
 	Atom2005HttpUri        = "http://www.w3.org/2005/Atom"
 	AtomPublishingHttpsUri = "https://www.w3.org/2007/app"
 	AtomPublishingHttpUri  = "http://www.w3.org/2007/app"
+	Roliens                = "urn:ietf:params:xml:ns:rolie-1.0"
 )
 
 type RolieRootElement interface {
@@ -17,17 +18,38 @@ type RolieRootElement interface {
 
 // Prepare xml root (top-level) element for marshaling
 func (s *Service) MarshalXMLRootPrepare() {
-	s.Xmlns = AtomPublishingHttpsUri
+	if s.Xmlns == "" {
+		s.Xmlns = AtomPublishingHttpsUri
+	}
+	if s.Atomns == "" {
+		s.Atomns = Atom2005HttpsUri
+	}
 }
 
 // Prepare xml root (top-level) element for marshaling
 func (f *Feed) MarshalXMLRootPrepare() {
-	f.Xmlns = Atom2005HttpsUri
+	if f.Xmlns == "" {
+		f.Xmlns = Atom2005HttpsUri
+	}
+	if f.Roliens == "" {
+		f.Roliens = Roliens
+	}
+	if f.Lang == "" {
+		f.Lang = "en-US"
+	}
 }
 
 // Prepare xml root (top-level) element for marshaling
 func (e *Entry) MarshalXMLRootPrepare() {
-	e.Xmlns = Atom2005HttpsUri
+	if e.Xmlns == "" {
+		e.Xmlns = Atom2005HttpsUri
+	}
+	if e.Roliens == "" {
+		e.Roliens = Roliens
+	}
+	if e.Lang == "" {
+		e.Lang = "en-US"
+	}
 }
 
 func AssertAtomNamespace(namespace string) error {
